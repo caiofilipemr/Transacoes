a6202cdb3aeb2bad005c21e66f613a3d159362a3
caiofilipemr <caiofilipemr@gmail.com>
2017-04-03 18:35:45 -0300

    Testes com binder

--- Transacoes/src/main/java/com/transacoes/TransacoesApplication.java
+++ Transacoes/src/main/java/com/transacoes/TransacoesApplication.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 @SpringBootApplication
 public class TransacoesApplication {
